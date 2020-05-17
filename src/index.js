2ca62bef6c23e0fd67c78afc6db169af0e74420a
Sven Helmberger <fforw@gmx.de>
2020-05-17 21:25:37 +0200

    First working version
    Removes edges from triangles making them quads, avoiding the outer
    edges.
    
    Overall we need a better graph model. This typed array was premature.

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function calculateNumberOfFaces(limit)
 
 function createHexagonalTiles(limit)
 {
-    const faces = new Float32Array(LENGTH);
+    const faces = new Float64Array(LENGTH);
 
     let off = 0;
 
@@ -81,27 +81,27 @@ function createHexagonalTiles(limit)
             {
                 if (j & 1)
                 {
-                    faces[off++] = pos.x;
-                    faces[off++] = pos.y;
-                    faces[off++] = pos.x + v1.x;
-                    faces[off++] = pos.y + v1.y;
-                    faces[off++] = pos.x + v2.x;
-                    faces[off++] = pos.y + v2.y;
+                    faces[off++] = (pos.x)|0;
+                    faces[off++] = (pos.y)|0;
+                    faces[off++] = (pos.x + v1.x)|0;
+                    faces[off++] = (pos.y + v1.y)|0;
+                    faces[off++] = (pos.x + v2.x)|0;
+                    faces[off++] = (pos.y + v2.y)|0;
                     off += 2;
                     faces[off++] = 3;
                     faces[off++] = 1;
-
+                    
                     pos.add(v2);
                 }
                 else
                 {
 
-                    faces[off++] = pos.x;
-                    faces[off++] = pos.y;
-                    faces[off++] = pos.x + v0.x;
-                    faces[off++] = pos.y + v0.y;
-                    faces[off++] = pos.x + v1.x;
-                    faces[off++] = pos.y + v1.y;
+                    faces[off++] = (pos.x)|0;
+                    faces[off++] = (pos.y)|0;
+                    faces[off++] = (pos.x + v0.x)|0;
+                    faces[off++] = (pos.y + v0.y)|0;
+                    faces[off++] = (pos.x + v1.x)|0;
+                    faces[off++] = (pos.y + v1.y)|0;
                     off += 2;
                     faces[off++] = 3;
                     faces[off++] = count === limit ? 2 : 0;
@@ -122,15 +122,26 @@ function createHexagonalTiles(limit)
 const faces = createHexagonalTiles(LIMIT);
 
 
-function findOtherEdge(faces, x0, y0, x1, y1, out)
+function findOtherEdge(faces, x0, y0, x1, y1, index, out)
 {
     for (let i = 0; i < faces.length; i += SIZE)
     {
-        const count = faces[i];
+        if (i === index)
+        {
+            continue;
+        }
 
+        // console.log("find", x0, y0, x1, y1, ":",
+        //     faces[i    ], faces[i + 1],
+        //     faces[i + 2], faces[i + 3],
+        //     faces[i + 4], faces[i + 5],
+        //     faces[i + 6], faces[i + 7],
+        // );
+
+        const count = faces[i + 8];
         if (
-            faces[i + 2] === x1 && faces[i + 3] === y1 &&
-            faces[i + 4] === x0 && faces[i + 5] === y0
+            faces[i    ] === x1 && faces[i + 1] === y1 &&
+            faces[i + 2] === x0 && faces[i + 3] === y0
         )
         {
             out.index = i;
@@ -138,8 +149,8 @@ function findOtherEdge(faces, x0, y0, x1, y1, out)
             return;
         }
         if (
-            faces[i + 4] === x1 && faces[i + 5] === y1 &&
-            faces[i + 6] === x0 && faces[i + 7] === y0
+            faces[i + 2] === x1 && faces[i + 3] === y1 &&
+            faces[i + 4] === x0 && faces[i + 5] === y0
         )
         {
             out.index = i;
@@ -147,11 +158,11 @@ function findOtherEdge(faces, x0, y0, x1, y1, out)
             return;
         }
 
-        if (count === 2)
+        if (count === 3)
         {
             if (
-                faces[i + 6] === x1 && faces[i + 7] === y1 &&
-                faces[i + 2] === x0 && faces[i + 3] === y0
+                faces[i + 4] === x1 && faces[i + 5] === y1 &&
+                faces[i    ] === x0 && faces[i + 1] === y0
             )
             {
                 out.index = i;
@@ -162,8 +173,8 @@ function findOtherEdge(faces, x0, y0, x1, y1, out)
         else
         {
             if (
-                faces[i + 6] === x1 && faces[i + 7] === y1 &&
-                faces[i + 8] === x0 && faces[i + 9] === y0
+                faces[i + 4] === x1 && faces[i + 5] === y1 &&
+                faces[i + 6] === x0 && faces[i + 7] === y0
             )
             {
                 out.index = i;
@@ -172,8 +183,8 @@ function findOtherEdge(faces, x0, y0, x1, y1, out)
             }
 
             if (
-                faces[i + 8] === x1 && faces[i + 9] === y1 &&
-                faces[i + 2] === x0 && faces[i + 3] === y0
+                faces[i + 6] === x1 && faces[i + 7] === y1 &&
+                faces[i    ] === x0 && faces[i    ] === y0
             )
             {
                 out.index = i;
@@ -190,18 +201,49 @@ function findOtherEdge(faces, x0, y0, x1, y1, out)
 const out = {index: -1, edge: 0};
 
 
+function printFace(faces, index)
+{
+    return (
+        faces[index    ] + "," + faces[index + 1] + "|" +
+        faces[index + 2] + "," + faces[index + 3] + "|" +
+        faces[index + 4] + "," + faces[index + 5]
+    );
+}
+
+const markedEdges = [];
+
+function mark(faces, index, edge)
+{
+    if (edge === 2)
+    {
+        markedEdges.push(
+            faces[index + edge*2], faces[index + edge*2 + 1],
+            faces[index         ], faces[index          + 1]
+        )
+    }
+    else
+    {
+        markedEdges.push(
+            faces[index + edge*2], faces[index + edge*2 + 1],
+            faces[index + (edge+1)*2], faces[index + (edge+1)*2 + 1]
+        )
+    }
+}
+
+
 function removeRandomEdges(faces, count)
 {
-    while (count > 0)
+    let success = 0;
+    for (let i = 0 ; i < count ; i++)
     {
-        const index = 0;//((Math.random() * NUM_FACES)|0) * SIZE;
-        if (faces[index] === 2)
+        const index = ((Math.random() * NUM_FACES)|0) * SIZE;
+        if (faces[index + 8] === 3)
         {
-            const caseMask = faces[index + 1];
+            const caseMask = faces[index + 9];
             const targetIsOddFace = !!(caseMask & 1);
             const targetIsOutmostFace = !!(caseMask & 2);
 
-            const edge = 0;//(Math.random() * 3)|0;
+            const edge = (Math.random() * 3)|0;
 
             if (!(targetIsOutmostFace && edge === 1))
             {
@@ -211,48 +253,102 @@ function removeRandomEdges(faces, count)
                 const x1 = edge === 2 ? faces[ index ]: faces[ index + (edge+1) * 2];
                 const y1 = edge === 2 ? faces[ index + 1]: faces[ index + (edge+1) * 2 + 1];
 
-                findOtherEdge(faces, x0,y0,x1,y1, out)
+
+                findOtherEdge(faces, x0,y0,x1,y1, index, out)
+                if (out.index >= 0 && faces[out.index + 8] === 3)
+                {
+                    const { index : otherIndex, edge: otherEdge } = out;
+                    const x2 = edge === 0 ? faces[ index + 2 * 2] : faces[ index + (edge - 1) * 2];
+                    const y2 = edge === 0 ? faces[ index + 2 * 2 + 1]: faces[ index + (edge - 1) * 2 + 1];
 
 
+                    faces[otherIndex + 8] = 4;
+                    switch(otherEdge)
+                    {
+                        case 2:
+                            faces[otherIndex + 6] = x2;
+                            faces[otherIndex + 7] = y2;
+                            break;
+                        case 1:
+                            faces[otherIndex + 6] = faces[otherIndex + 4];
+                            faces[otherIndex + 7] = faces[otherIndex + 5];
+                            faces[otherIndex + 4] = x2;
+                            faces[otherIndex + 5] = y2;
+                            break;
+                        case 0:
+                            faces[otherIndex + 6] = faces[otherIndex + 4];
+                            faces[otherIndex + 7] = faces[otherIndex + 5];
+                            faces[otherIndex + 4] = faces[otherIndex + 2];
+                            faces[otherIndex + 5] = faces[otherIndex + 3];
+                            faces[otherIndex + 2] = x2
+                            faces[otherIndex + 3] = y2;
+                            break;
+                    }
+                    faces[index + 8] = 0;
+                    //mark(faces, index,edge)
 
-                // if (out.index < 0)
-                // {
-                //     throw new Error("Could not find other edge")
-                // }
-
-                console.log("Face #",index, ", edge = ", edge, " paired with  Face #" + out.index, ", edge = ", out.edge)
-
-                count--;
+                    console.log("Face #", printFace(faces,index), ", edge = ", edge, " paired with  Face #" + printFace(faces,out.index), ", edge = ", out.edge)
+                    success++;
+                }
             }
         }
     }
+
+    console.log("Success:", success, "out of", count);
+
+    return success;
 }
 
 
-//removeRandomEdges(faces, NUM_EDGES * 0.2)
+//removeRandomEdges(faces, NUM_EDGES * 0.25)
+//subdivide(faces);
+
+
 
 function mainLoop()
 {
+    ctx.strokeStyle = "#f0f";
+
+    // for (let i = 0; i < markedEdges.length; i+=4)
+    // {
+    //     const x0 = markedEdges[i];
+    //     const y0 = markedEdges[i+1];
+    //     const x1 = markedEdges[i+2];
+    //     const y1 = markedEdges[i+3];
+    //
+    //     ctx.beginPath();
+    //     ctx.moveTo(x0,y0);
+    //     ctx.lineTo(x1,y1);
+    //     ctx.stroke();
+    //
+    // }
+
     for (let pos = 0; pos < LENGTH; pos += SIZE)
     {
         const count = faces[pos + 8];
-        const caseMask = faces[pos + 9];
-        const targetIsOddFace = !!(caseMask & 1);
-        const targetIsOutmostFace = !!(caseMask & 2);
 
-        ctx.strokeStyle = targetIsOutmostFace ? "#0f0" : "#800";
-
-        ctx.beginPath();
-        ctx.moveTo(faces[pos ],faces[pos + 1]);
-
-        for (let i = 1; i < count; i++)
+        if (count >= 3)
         {
-            ctx.lineTo(faces[pos + i*2],faces[pos +  i*2 + 1]);
-        }
+            const caseMask = faces[pos + 9];
+            const targetIsOddFace = !!(caseMask & 1);
+            const targetIsOutmostFace = !!(caseMask & 2);
 
-        ctx.closePath();
-        ctx.stroke();
+            ctx.strokeStyle = "#800";
+
+            ctx.beginPath();
+            ctx.moveTo(faces[pos ],faces[pos + 1]);
+
+            for (let i = 1; i < count; i++)
+            {
+                ctx.lineTo(faces[pos + i*2],faces[pos +  i*2 + 1]);
+            }
+
+            ctx.closePath();
+            ctx.stroke();
+
+        }
     }
+
 
 //    raf(mainLoop)
 }
